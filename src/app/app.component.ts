adcd8a14519d277ce74f1d10cb39b6f638797520
Manuel.Mesa <Manuel.Mesa@ngkf.com>
2019-10-17 17:12:27 -0400

    used angular components and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import{ProductService} from './product.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <h1>{{title}}</h1>
+  <products></products>
+  `,
+  providers:[ProductService]
 })
 export class AppComponent {
-  title = 'BeginningAngular';
+  title = 'My First Angular App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ProductsComponent } from './products.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
